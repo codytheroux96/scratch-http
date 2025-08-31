3c06154877f62741cc32c6a258ff103d822d597b
codytheroux96 <cody.theroux3@gmail.com>
2025-08-30 22:52:12 -0400

    parsing a text file line by line

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("msg.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	str := ""
+
+	for {
+		data := make([]byte, 8)
+
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i + 1:]
+
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+
+		str += string(data)
+	}
+
+	if len(str) != 0 {
+		fmt.Printf("read: %s\n", str)
+	}
+}
